758cceba257bf26e2db38569948d765f37a772bf
Yillié <yilliee@proton.me>
2025-06-09 12:44:45 +0500

    fix(ui): Force TV Boxes to use TabletUI

--- app/src/main/java/eu/kanade/presentation/util/WindowSize.kt
+++ app/src/main/java/eu/kanade/presentation/util/WindowSize.kt
@@ -3,10 +3,11 @@ package eu.kanade.presentation.util
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import eu.kanade.tachiyomi.util.system.isTabletUi
 
 @Composable
 @ReadOnlyComposable
 fun isTabletUi(): Boolean {
-    return LocalConfiguration.current.isTabletUi()
+    return LocalConfiguration.current.isTabletUi(LocalContext.current)
 }
